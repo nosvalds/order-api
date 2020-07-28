30dd8ac653a275eac4e49aecf359c62ac9211a21
Nik Osvalds <nosvalds@gmail.com>
2020-07-28 18:22:21 +0200

    write failing db test for Order

--- tests/Unit/OrderTest.php
+++ tests/Unit/OrderTest.php
@@ -17,22 +17,40 @@ class OrderTest extends TestCase
     public function testFillable()
     {
         $order = new Order([
-            "delivery_postcode" => "Bilbo",
-            "order_description" => "Baggins",
+            "delivery_postcode" => "BS1 5DP",
+            "order_description" => "2 barrels of wine",
             "price" => 1.99,
             "danger" => "Aaaargh!",
         ]);
 
         // delivery_postcode should be set, as it's in $fillable
-        $this->assertSame("Bilbo", $order->delivery_postcode);
+        $this->assertSame("BS1 5DP", $order->delivery_postcode);
 
         // order_description should be set, as it's in $fillable
-        $this->assertSame("Baggins", $order->order_description);
+        $this->assertSame("2 barrels of wine", $order->order_description);
 
         // price should be set, as it's in $fillable
         $this->assertSame(1.99, $order->price);
 
         // danger shouldn't be set, as it's not in $fillable
         $this->assertSame(null, $order->danger);
+    }
+
+    public function testOrderDatabase()
+    {
+        Order::create([
+            "delivery_postcode" => "BS1 5DP",
+            "order_description" => "2 barrels of wine",
+            "price" => 1.99,
+        ]);
+
+        // get the first Order back from the database
+        $orderFromDB = Order::all()->first();
+
+        // check the postcodes match
+        $this->assertSame("BS1 5DP", $orderFromDB->delivery_postcode);
+
+        // check the price matches
+        $this->assertSame(1.99, $orderFromDB->price);
     }
 }
