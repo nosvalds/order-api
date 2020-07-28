e31320d6ab4771d8f1502670ddfb9304c915a316
Nik Osvalds <nosvalds@gmail.com>
2020-07-28 18:07:56 +0200

    write failing customer database test

--- tests/Unit/CustomerTest.php
+++ tests/Unit/CustomerTest.php
@@ -30,4 +30,19 @@ class CustomerTest extends TestCase
             // danger shouldn't be set, as it's not in $fillable
             $this->assertSame(null, $customer->danger);
         }
+
+    public function testCustomerDatabase()
+    {
+        Customer::create([
+            "first_name" => "Bilbo",
+            "last_name" => "Baggins",
+            "email" => "email@fun.com",
+        ]);
+
+        // get the first customer back from the database
+        $customerFromDB = Customer::all()->first();
+
+        // check the titles match
+        $this->assertSame("Bilbo", $customerFromDB->first_name);
+    }
 }
