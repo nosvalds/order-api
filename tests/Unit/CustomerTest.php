1cceeeafa9bb981ac3c802c6991fcb9809fc57d3
Nik Osvalds <nosvalds@gmail.com>
2020-07-28 17:56:21 +0200

    first customer tests written for fillable columns

--- tests/Unit/CustomerTest.php
+++ tests/Unit/CustomerTest.php
@@ -2,17 +2,31 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CustomerTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $this->assertTrue(true);
-    }
+    use RefreshDatabase;
+
+    public function testFillable()
+        {
+            $customer = new Customer([
+                "first_name" => "Bilbo",
+                "last_name" => "Baggins",
+                "email" => "email@fun.com",
+                "danger" => "Aaaargh!",
+            ]);
+            // first_name should be set, as it's in $fillable
+            $this->assertSame("Bilblo", $customer->first_name);
+
+            // last_name should be set, as it's in $fillable
+            $this->assertSame("Baggins", $customer->last_name);
+
+            // email should be set, as it's in $fillable
+            $this->assertSame("email@fun.com", $customer->email);
+
+            // danger shouldn't be set, as it's not in $fillable
+            $this->assertSame(null, $customer->danger);
+        }
 }
